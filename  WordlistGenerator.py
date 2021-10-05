88b52db014e4c76f86a409c1d4b6ba05886639c7
f4n0 <36199751+f4n0@users.noreply.github.com>
2021-10-05 20:51:18 +0000

    removed comments

---  WordlistGenerator.py
+++  WordlistGenerator.py
@@ -1,6 +1,3 @@
-#WPA2 for FritzBox is 16 Numeric lenght
-#All the passwords never have 3 same consecutive digits
-#Inspired by rwx-777 (firstly written in perl, I was just learning py)
 import re
 for i in range(1000000000000,9999000000000000):
     num = ('%0.16d' % i)
